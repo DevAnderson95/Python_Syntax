9c12800541dd343660e9edc6e7f2b3ab089a20e6
DevAnderson95 <DevAnderson95@gmail.com>
2024-12-02 04:24:52 -0500

    first commit

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,17 @@
+# Task 1 
+milk = 4.14
+eggs = 4.98
+bread = 2.09
+
+print("The price of milk, eggs, and bread is", + milk + eggs + bread)
+
+
+# Task 2 
+savings = 5000
+interest = .05
+
+yearly_interest = savings * interest
+
+yearly_statement = yearly_interest + savings
+
+print(yearly_statement)
---
+++ Naming_Convention.py
@@ -0,0 +1,5 @@
+# Task 1
+PI_VALUE = 3.14
+UserAge = 25
+User_Location = "New York"
+MAX_LIMIT = 1000
